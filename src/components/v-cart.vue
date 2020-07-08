bd9dd69e92be7db19d1f5faf2b244b08976c6579
Oleh Zankic <firegames5645@gmail.com>
2020-07-08 17:35:20 +0300

    lessons 3/8

---
+++ src/components/v-cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="v-cart"></div>
+</template>
+
+<script>
+export default {
+  name: "v-cart",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="v-catalog-item">
+    <img src alt="img" />
+    <p>Item 1</p>
+    <p>Price: 100</p>
+    <button>Add to cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-catalog-item",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.v-catalog-item {
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 #e0e0e0;
+  padding: 1rem;
+  margin: 1rem;
+}
+</style>
